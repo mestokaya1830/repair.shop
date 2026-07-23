6818fabb57db34dbbd831a3b0b8efdf153267c90
mestokaya <mesto1830@outlook.com>
2026-07-23 13:03:17 +0200

    bgbgbbg

--- server/src/middleware/repair.number.middleware.js
+++ server/src/middleware/repair.number.middleware.js
@@ -1,7 +1,13 @@
-import { repairNumber } from "../utils/repair.number.js";
-
 const createRepairNumber = (req, res, next) => {
-  req.repairNumber = repairNumber();
+  const date = new Date();
+
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const random = Math.floor(1000 + Math.random() * 9000);
+
+  req.repairNumber = `REQ-${year}${month}${day}-${random}`;
+
   next();
 };
 
